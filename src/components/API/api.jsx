c5aa3a7a8da06a007083b5ba48e2aa218e7faa8b
charles806 <c08445333@gmail.com>
2025-10-30 13:03:12 +0100

    i

--- src/components/API/api.jsx
+++ src/components/API/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = "https://stackmentor-backend-corq.onrender.com/api";
 
 // Create axios instance
 const api = axios.create({
